df94e70f818d9e422f63dacf16e3584f9fe459f6
Matias N Reyes <62193154+matnreyes@users.noreply.github.com>
2022-09-14 12:16:42 -0400

    restructured course object to be more complex, changed they info passed onto Total to appease exercise guidelines

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -20,34 +20,38 @@ const Content = (props) => {
   )
 }
 
-const Total = (props) => (
-  <>
-    <p>Number of exercises {props.total}</p>
-  </>
-)
+const Total = (props) => {
+  const total = props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises
+  return (
+    <>
+      <p>Number of exercises {total}</p>
+    </>
+  )
+}
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
-
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total total={parts[0].exercises + parts[1].exercises + parts[2].exercises} />
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
     </div>
   )
 }
