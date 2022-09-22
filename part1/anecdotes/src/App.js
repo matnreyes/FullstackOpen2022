7f18544ac293f76ca05adaf3ffd34230db70cde2
Matias N Reyes <62193154+matnreyes@users.noreply.github.com>
2022-09-22 17:53:35 -0400

    voting system implemented
    users can now vote on their favorite anecdote,
    State has been restructured

--- part1/anecdotes/src/App.js
+++ part1/anecdotes/src/App.js
@@ -1,23 +1,55 @@
 import { useState } from 'react'
 
-const App = () => {
-  const anecdotes = [
-    'If it hurts, do it more often.',
-    'Adding manpower to a late software project makes it later!',
-    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
-    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
-    'Premature optimization is the root of all evil.',
-    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
-    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.'
-  ]
-
+const App = () => {  
   const [selected, setSelected] = useState(0)
+  const [anecdotes, setAnecdotes] = useState([
+      {
+        anecdote: 'If it hurts, do it more often.',
+        votes: 0
+      },
+      {
+        anecdote: 'Adding manpower to a late software project makes it later!',
+        votes: 0
+      },
+      {
+        anecdote: 'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+        votes: 0
+      },
+      {
+        anecdote: 'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+        votes: 0
+      },
+      {
+        anecdote: 'Premature optimization is the root of all evil.',
+        votes: 0
+      },
+      {
+        anecdote: 'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+        votes: 0
+      },
+      {
+        anecdote: 'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+        votes: 0
+      }
+    ]
+  )
 
   const generateRandom = () =>  Math.floor(Math.random() * anecdotes.length)
+  
+  const handleVote = () => {
+    const updateVotes = () => {
+      const anecdoteCopy = [...anecdotes] 
+      anecdoteCopy[selected].votes +=  1
+      setAnecdotes(anecdoteCopy)
+    }
+    return updateVotes
+  }
    
   return (
     <div>
-      <p>{anecdotes[selected]}</p>
+      <p>{anecdotes[selected].anecdote}</p>
+      <p>has {anecdotes[selected].votes} votes</p>
+      <button onClick={handleVote()}>vote</button>
       <button onClick={() => setSelected(generateRandom)}>next anecdote</button>
     </div>
   )
