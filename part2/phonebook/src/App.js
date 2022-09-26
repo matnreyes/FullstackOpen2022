8ab174110a810345eee5cb6b666d3aeb7e72e3d7
Matias N Reyes <62193154+matnreyes@users.noreply.github.com>
2022-09-26 18:42:04 -0400

    phonebook accepts phone numbers

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,21 +1,25 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    {name: 'Arto Hellas'}
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
 
   const handleNewName = (e) => {
     setNewName(e.target.value)
+  }
+ 
+  const handleNewNumber = (e) => {
+    setNewNumber(e.target.value)
   }
 
   const handleSubmit = (e) => {
     e.preventDefault()
     findDupe(newName)
     ? alert(`${newName} is already in the phonebook`)
-    : setPersons(persons.concat({name: newName}))
+    : setPersons(persons.concat({name: newName, number: newNumber}))
     setNewName('')
+    setNewNumber('')
   }
 
   const findDupe = (newName) => persons.some(p => p.name === newName)
@@ -24,16 +28,15 @@ const App = () => {
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
-        <div>
-          name: <input value={newName} onChange={handleNewName}/>
-        </div>
+        <div>name: <input value={newName} onChange={handleNewName}/></div>
+        <div>number: <input value={newNumber} onChange={handleNewNumber}/></div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+        {persons.map(person => <li key={person.name}>{person.name} {person.number}</li>)}
       </ul>
     </div>
   )
