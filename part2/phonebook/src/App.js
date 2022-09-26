7cd84d9075de0b207115e61383d356de2d569e8a
Matias N Reyes <62193154+matnreyes@users.noreply.github.com>
2022-09-26 18:48:41 -0400

    added search

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,9 +1,15 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+  const [newSearch, setNewSearch] = useState('')
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
+  ])
 
   const handleNewName = (e) => {
     setNewName(e.target.value)
@@ -22,11 +28,17 @@ const App = () => {
     setNewNumber('')
   }
 
+  const handleSearch = (e) => {
+    setNewSearch(e.target.value)
+  }
+
   const findDupe = (newName) => persons.some(p => p.name === newName)
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <div>filter shown with<input value={newSearch} onChange={handleSearch}/></div>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>name: <input value={newName} onChange={handleNewName}/></div>
         <div>number: <input value={newNumber} onChange={handleNewNumber}/></div>
