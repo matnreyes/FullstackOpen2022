61bf61c3e9901cc8384072c3b3b98e62194a7d5d
Matias N Reyes <62193154+matnreyes@users.noreply.github.com>
2022-09-26 17:10:03 -0400

    new names can be added
    Users can use form to save new names into the phonebook

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
 
-function App() {
+const App = () => {
+  const [persons, setPersons] = useState([
+    {name: 'Arto Hellas'}
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleNewName = (e) => {
+    setNewName(e.target.value)
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setPersons(persons.concat({name: newName}))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input value={newName} onChange={handleNewName}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <ul>
+        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+      </ul>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
