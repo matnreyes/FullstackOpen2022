4b182d3a30580e7c480db2cd9f4c1658f5fd5412
matnreyes <matias_reyes@icloud.com>
2022-10-31 15:33:22 -0400

    changed endpoint for api

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:3001/persons'
+const baseURL = 'http://localhost:3001/api/contacts'
 
 const getAll = () => {
     const getResponse = axios.get(baseURL)
