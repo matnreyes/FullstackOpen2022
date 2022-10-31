418934e390a15071a4dec32db71266a7b53d6040
matnreyes <matias_reyes@icloud.com>
2022-10-31 15:33:50 -0400

    cors support

--- part3/phonebook_backend/index.js
+++ part3/phonebook_backend/index.js
@@ -1,9 +1,11 @@
 const express = require ('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
 morgan.token('body', (req, res) => JSON.stringify(req.body))
 
+app.use(cors())
 app.use(express.json())
 app.use(morgan((tokens, req, res) => [
     tokens.method(req, res),
