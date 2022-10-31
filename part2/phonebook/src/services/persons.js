1806239a5f511e131aac8c1af68c8320eae61f3a
matnreyes <matias_reyes@icloud.com>
2022-10-31 15:45:53 -0400

    front end relative url

--- part2/phonebook/src/services/persons.js
+++ part2/phonebook/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseURL = 'http://localhost:3001/api/contacts'
+const baseURL = '/api/contacts'
 
 const getAll = () => {
     const getResponse = axios.get(baseURL)
