6872565f8dc9726dfe115190917b9504636252b1
matnreyes <matias_reyes@icloud.com>
2022-11-02 15:44:06 -0400

    mongo.js shows all contacts and can add new contacts

--- part3/phonebook_backend/mongo.js
+++ part3/phonebook_backend/mongo.js
@@ -1,7 +1,16 @@
 const mongoose = require('mongoose')
 
+const params = process.argv.length
+
+if (params < 3) {
+    console.log('Please enter your password')
+    process.exit(1)
+}
+
 const password = process.argv[2]
 const url = `mongodb+srv://admin:${password}@cluster0.ev0ug1n.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+mongoose.connect(url)
 
 const contactSchema = new mongoose.Schema({
     name: String,
@@ -10,18 +19,25 @@ const contactSchema = new mongoose.Schema({
 
 const Contact = mongoose.model('Contact', contactSchema)
 
-
-mongoose
-.connect(url)
-.then(result => {
-        const contact = new Contact({
-            name: process.argv[3],
-            number: process.argv[4]
+if (params < 5) {
+    Contact
+        .find({})
+        .then(results => {
+            console.log('phonebook:')
+            results.forEach(result => {
+                console.log(result.name, result.number)
+            })
+            mongoose.connection.close()
+            process.exit(0)
         })
+}
 
-        return contact.save()
-    })
-    .then(() => {
-        return mongoose.connection.close()
-    })
-    .catch(err => console.log(err))
+const contact = new Contact({
+    name: process.argv[3],
+    number: process.argv[4]
+})
+
+contact.save().then(() => {
+    console.log(`added ${contact.name} ${contact.number} to phonebook`)
+    mongoose.connection.close()
+})
