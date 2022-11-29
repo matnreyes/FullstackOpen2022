55864767cc02afff2c2df4b72c70bde0a627a38f
matnreyes <matias_reyes@icloud.com>
2022-11-29 11:29:32 -0500

    error logging middleware

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -4,36 +4,14 @@ require('dotenv').config()
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-
-const Blog = require('./models/blog')
+const blogsRouter = require('./controllers/blogs')
 
 mongoose.connect(process.env.MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-  Blog.find({})
-    .then((blogs) => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const body = request.body
-
-  const blog = new Blog({
-    title: body.title,
-    author: body.author,
-    url: body.url,
-    likes: body.likes
-  })
-
-  blog.save()
-    .then((result) => {
-      response.status(201).json(result)
-    })
-})
+app.use('/', blogsRouter)
 
 const PORT = process.env.PORT || 3003
 app.listen(PORT, () => {
--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -1,5 +1,4 @@
 const blogsRouter = require('express').Router()
-const { models } = require('mongoose')
 const Blog = require('../models/blog')
 
 blogsRouter.get('/', (req, res, next) => {
