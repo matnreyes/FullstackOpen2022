23f3d3b070206cc07afaba653c8bb9ff2d892bed
matnreyes <matias_reyes@icloud.com>
2022-11-29 14:34:04 -0500

    total likes function;

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,5 +1,13 @@
+// eslint-disable-next-line no-unused-vars
 const dummy = (blogs) => 1
 
+const totalLikes = (blogs) => {
+  const reducer = (sum, item) => sum + item
+
+  return (blogs.map((blog) => blog.likes)).reduce(reducer, 0)
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
