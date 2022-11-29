1b5ded1bc52b2822b92169eb11eac3d08e3d2523
matnreyes <matias_reyes@icloud.com>
2022-11-29 14:40:14 -0500

    fake blogs test likes

--- part4/bloglist/tests/list_helper.test.js
+++ part4/bloglist/tests/list_helper.test.js
@@ -1,8 +1,18 @@
 const listHelper = require('../utils/list_helper')
+const testBlogs = require('../utils/testblogs')
 
 test('dummy returns one', () => {
   const blogs = []
 
   const result = listHelper.dummy(blogs)
   expect(result).toBe(1)
+})
+
+describe('total likes', () => {
+  const listWithOneBlog[0] = testBlogs[0]
+
+  test('when list has ony one blog, equals the like od that', () => {
+    const result = listHelper.totalLikes(listWithOneBlog)
+    expect(result).toBe(5)
+  })
 })
