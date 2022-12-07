ba1f78efbab126834ca94117a4da5fc4a74bb52b
matnreyes <matias_reyes@icloud.com>
2022-12-07 18:30:12 -0500

    app only shows blogs when signed in

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -30,36 +30,48 @@ const App = () => {
   }
 
   const loginForm = () => (
-    <form onSubmit={handleLogin}>
-      <div>
-        username
-        <input 
-        type='text'
-        value={username}
-        name='username'
-        onChange={({ target }) => setUsername(target.value)}
-        />
-      </div>
-      <div>
-        password
-        <input
-        type='password'
-        value={password}
-        name='password'
-        onChange={({ target }) => setPassword(target.value)}
-        />
-      </div>
-      <button type='submit'>login</button>
-    </form>
+    <div>
+      <h2>log in to application</h2>
+      <form onSubmit={handleLogin}>
+        <div>
+          username
+          <input 
+          type='text'
+          value={username}
+          name='username'
+          onChange={({ target }) => setUsername(target.value)}
+          />
+        </div>
+        <div>
+          password
+          <input
+          type='password'
+          value={password}
+          name='password'
+          onChange={({ target }) => setPassword(target.value)}
+          />
+        </div>
+        <button type='submit'>login</button>
+      </form>
+    </div>
+  )
+
+  const blogDisplay = () => (
+    <>
+      <h2>blogs</h2>
+      <p>Logged in as {user.username}</p>
+      {blogs.map(blog => 
+        <Blog key={blog.id} blog={blog}/>
+      )}
+    </>
   )
 
   return (
     <div>
-      {user === null && loginForm()}
-      <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+      {user === null ?
+      loginForm() :
+      blogDisplay()
+      }
     </div>
   )
 }
