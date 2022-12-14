831987ea44c969e3f7d06107802f30fdcc681417
matnreyes <matias_reyes@icloud.com>
2022-12-14 15:49:43 -0500

    added a static build that only runs when in production mode

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -22,6 +22,10 @@ mongoose.connect(config.MONGODB_URI)
     logger.error(error)
   })
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('build'))
+}
+
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
