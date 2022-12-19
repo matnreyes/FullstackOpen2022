f920a0814fbfacfc907300b3e3a5edf15220af3d
matnreyes <matias_reyes@icloud.com>
2022-12-19 01:13:16 -0500

    rendering tests

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,22 +1,47 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 
-test('<Blog /> only rredners title and author', () => {
-  const blog = {
-    title: 'testBlog',
-    author: 'testAuthor',
-    url: 'opjndf.com',
-    user: {
-      username: 'testUser'
+
+describe('<Blog />', () => {
+  test('only redners title and author', () => {
+    const blog = {
+      title: 'testBlog',
+      author: 'testAuthor',
+      url: 'opjndf.com',
+      user: {
+        username: 'testUser'
+      }
     }
-  }
 
-  const { container } = render(<Blog blog={blog} />)
-  const div = container.querySelector('.moreInfo')
+    const { container } = render(<Blog blog={blog} />)
+    const div = container.querySelector('.moreInfo')
 
 
-  expect(div).toHaveStyle('display: none')
+    expect(div).toHaveStyle('display: none')
+  })
+
+  test('renders likes and url when button is clicked', async () => {
+    const blog = {
+      title: 'testBlog',
+      author: 'testAuthor',
+      url: 'opjndf.com',
+      user: {
+        username: 'testUser'
+      }
+    }
+
+    const { container } = render(<Blog blog={blog} />)
+
+    const user = userEvent.setup()
+    const div = container.querySelector('.moreInfo')
+    const button = screen.getByText('expand')
+
+    await user.click(button)
+
+    expect(div).not.toHaveStyle('display: none')
+  })
 })
