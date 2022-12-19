20ac178a23a9f45c4fae4288f3f06999613d2515
matnreyes <matias_reyes@icloud.com>
2022-12-19 03:18:09 -0500

    added test for like button

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -44,4 +44,37 @@ describe('<Blog />', () => {
 
     expect(div).not.toHaveStyle('display: none')
   })
+
+  test('like button is clicked twice, two likes', async () => {
+    const blog = {
+      title: 'testBlog',
+      author: 'testAuthor',
+      url: 'opjndf.com',
+      user: {
+        username: 'testUser'
+      },
+      likes: 0
+    }
+    const mockHandler = jest.fn()
+
+    const { container } = render(<Blog blog={blog} sortBlogs={mockHandler}/>)
+
+
+    const user = userEvent.setup()
+    const expandButton = screen.getByText('expand')
+
+
+    await user.click(expandButton)
+
+
+    // Like blog twice
+    const likeButton = screen.getByText('like')
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+
+
+    const div = container.querySelector('.moreInfo')
+    expect(div).toHaveTextContent('likes: 2')
+  })
 })
