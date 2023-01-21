80c007bc145d1119cff9ac68dc98111e20429821
matnreyes <matias_reyes@icloud.com>
2023-01-21 13:29:59 -0500

    delete comments

--- part9/exerciseCalculator.ts
+++ part9/exerciseCalculator.ts
@@ -14,18 +14,6 @@ interface ActivityInfo {
 }
 
 const calculateExercises = (hours: Array<number>, target: number): ActivityInfo => {
-  // MUST calculate daily exercise hours
-  // Compares it to the target amount of daily hours 
-    /* 
-      Returns an object that includes:
-      - the number of days
-      - the number of training days
-      - the original target value
-      - the calculated average time
-      - boolean value describing if the target was reached
-      - rating between 1-3 that tells how the hours are met
-      - text value explaining the rating
-    */
   const periodLength = hours.length;
   const totalHours  = hours.reduce((a, b) => a + b, 0);
   const trainingDays = (hours.filter(h => h > 0)).length;
