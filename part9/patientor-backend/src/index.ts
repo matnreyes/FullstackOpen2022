273afb35541560559b19de2babd6c4661eda7cf9
matnreyes <matias_reyes@icloud.com>
2023-01-25 18:17:26 -0500

    add diagnoses route

--- part9/patientor-backend/src/index.ts
+++ part9/patientor-backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import diagnoseRouter from './routes/diagnoses';
 const app = express();
 app.use(express.json());
 app.use(cors());
@@ -7,6 +8,8 @@ app.use(cors());
 app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
+
+app.use('/api/diagnoses', diagnoseRouter);
 
 const PORT = 3001;
 
