7b74a09a68d33e886216d1c922deabaef40e6ae7
matnreyes <matias_reyes@icloud.com>
2023-01-25 18:51:17 -0500

    modify structure of db data to be imported better into services

--- part9/patientor-backend/data/diagnoses.ts
+++ part9/patientor-backend/data/diagnoses.ts
@@ -1,4 +1,6 @@
-[
+import { Diagnose } from "../src/types";
+
+const diagnoseData: Array<Diagnose> =[
   {
     "code": "M24.2",
     "name": "Disorder of ligament",
@@ -76,4 +78,6 @@
     "name": "Other proliferative retinopathy",
     "latin": "Alia retinopathia proliferativa"
   }
-]
+];
+
+export default diagnoseData;
