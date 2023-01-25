dab19ed6a04a4c0f0f99d3e69f98dc38e71f2f15
matnreyes <matias_reyes@icloud.com>
2023-01-24 22:17:59 -0500

    add frontend
    frontend and backend can now talk to each other

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-app.get('/ping', (_req, res) => {
+app.get('/api/ping', (_req, res) => {
   res.send('pong');
 });
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
