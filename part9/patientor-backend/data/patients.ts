f0e46760ed967c53d670972526283016c7f61fe1
matnreyes <matias_reyes@icloud.com>
2023-01-25 19:05:14 -0500

    modify data to import better into PatientService

--- part9/patientor-backend/data/patients.ts
+++ part9/patientor-backend/data/patients.ts
@@ -1,4 +1,6 @@
-[
+import { Patient } from '../src/types';
+
+const patients: Patient[] = [
   {
       "id": "d2773336-f723-11e9-8f0b-362b9e155667",
       "name": "John McClane",
@@ -39,4 +41,6 @@
       "gender": "male",
       "occupation": "Digital evangelist"
   }
-]
+];
+
+export default patients;
