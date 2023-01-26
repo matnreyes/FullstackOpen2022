15e4150845158c8efe15d54a0ae3f4c18256f97e
matnreyes <matias_reyes@icloud.com>
2023-01-25 19:06:44 -0500

    modify type to keep syntax the same

--- part9/patientor-backend/data/patients.ts
+++ part9/patientor-backend/data/patients.ts
@@ -1,6 +1,6 @@
 import { Patient } from '../src/types';
 
-const patients: Patient[] = [
+const patients: Array<Patient> = [
   {
       "id": "d2773336-f723-11e9-8f0b-362b9e155667",
       "name": "John McClane",
