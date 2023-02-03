dd91917387654b151d03406c85911e0a1266104b
matnreyes <matias_reyes@icloud.com>
2023-02-03 13:44:16 -0500

    add handler for new anecdote request

--- part6/query-anecdotes/src/services/anecdoteService.js
+++ part6/query-anecdotes/src/services/anecdoteService.js
@@ -4,3 +4,6 @@ const baseUrl = 'http://localhost:3001/anecdotes'
 
 export const getAll = () => 
   axios.get(baseUrl).then(res => res.data)
+
+export const createAnecdote = (content) => 
+  axios.post(baseUrl, content).then(res => res.data)
