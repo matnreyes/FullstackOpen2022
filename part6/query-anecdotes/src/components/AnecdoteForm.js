6885d65f2d29e2c95a52577f00a15e6305db6558
matnreyes <matias_reyes@icloud.com>
2023-02-03 13:57:22 -0500

    add functionality to create newAnecdote and update application state using react query

--- part6/query-anecdotes/src/components/AnecdoteForm.js
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -1,11 +1,22 @@
+import { createAnecdote } from '../services/anecdoteService'
+import { useMutation, useQueryClient } from 'react-query'
+
 const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+
+  const newAnecdoteMutation = useMutation(createAnecdote, {
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData('anecdotes')
+      queryClient.setQueryData('anecdotes', anecdotes.concat(newAnecdote))
+    }
+  })
 
   const onCreate = (event) => {
     event.preventDefault()
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
-    console.log('new anecdote')
-}
+    newAnecdoteMutation.mutate({ content })
+  }
 
   return (
     <div>
