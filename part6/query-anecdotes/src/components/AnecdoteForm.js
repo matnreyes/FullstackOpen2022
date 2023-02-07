fadf8de87c30d21dcd6ed4a0f2a17c964e67af45
matnreyes <matias_reyes@icloud.com>
2023-02-07 14:53:36 -0500

    add error handling for creating new notes

--- part6/query-anecdotes/src/components/AnecdoteForm.js
+++ part6/query-anecdotes/src/components/AnecdoteForm.js
@@ -6,10 +6,21 @@ const AnecdoteForm = () => {
   const queryClient = useQueryClient()
   const dispatchNotification = useNotificationDispatch()
 
+  const setNotifcation = (content) => {
+    dispatchNotification({ type: 'SET_NOTIFICATION', payload: content})
+    setTimeout(() => {
+      dispatchNotification({ type: 'RESET' })
+    }, 5000)
+  }
+
   const newAnecdoteMutation = useMutation(createAnecdote, {
     onSuccess: (newAnecdote) => {
       const anecdotes = queryClient.getQueryData('anecdotes')
-      queryClient.setQueryData('anecdotes', anecdotes.concat(newAnecdote))
+      queryClient.setQueryData('anecdotes', anecdotes.concat(newAnecdote.data.content))
+      setNotifcation(`Added: ${newAnecdote.data.content}`)
+    },
+    onError: ({ response }) => {
+      setNotifcation(`Error: ${response.data.error}`)
     }
   })
 
@@ -18,10 +29,6 @@ const AnecdoteForm = () => {
     const content = event.target.anecdote.value
     event.target.anecdote.value = ''
     newAnecdoteMutation.mutate({ content, votes: 0 })
-    dispatchNotification({ type: 'SET_NOTIFICATION', payload: `Added: ${content}`})
-    setTimeout(() => {
-      dispatchNotification({ type: 'RESET'})
-    }, 5000)
   }
 
   return (
--- part6/query-anecdotes/src/services/anecdoteService.js
+++ part6/query-anecdotes/src/services/anecdoteService.js
@@ -6,7 +6,7 @@ export const getAll = () =>
   axios.get(baseUrl).then(res => res.data)
 
 export const createAnecdote = (content) => 
-  axios.post(baseUrl, content).then(res => res.data)
+  axios.post(baseUrl, content).then(res => res)
 
 export const submitVote = (anecdote) => 
   axios.put(`${baseUrl}/${anecdote.id}`, {...anecdote, votes: anecdote.votes + 1}).then(res => res.data)
