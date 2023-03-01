59a00b9dd3fb7ea483f8e99629c3152ec01a66ea
matnreyes <matias_reyes@icloud.com>
2023-02-28 19:36:08 -0500

    add new types for Entry

--- part9/patientor/server/src/types.ts
+++ part9/patientor/server/src/types.ts
@@ -1,12 +1,56 @@
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
-export interface Entry {
-}
-
 export type Diagnose = {
   code: string;
   name: string;
   latin?: string;
 };
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnose['code']>
+}
+
+type SickLeaveTime = {
+  startDate: string,
+  endDate: string;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  specialist: string;
+  employerName: string;
+  sickLeave?: SickLeaveTime;
+}
+
+type Discharge = {
+  date: string,
+  criteria: string,
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  specialist: string;
+  discharge: Discharge;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry = 
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
 
 export type Patient = {
   id: string,
