9ea24d1d7146d6d277edbf6ea3d7904cc62fdfee
matnreyes <matias_reyes@icloud.com>
2023-03-02 13:23:33 -0500

    add addEntry function for new entries

--- part9/patientor/server/src/services/patientsService.ts
+++ part9/patientor/server/src/services/patientsService.ts
@@ -1,6 +1,6 @@
 import patients from '../../data/patients';
 import { v4 as uuid } from 'uuid';
-import { PublicPatient, Patient, NewPatient } from '../types';
+import { PublicPatient, Patient, NewPatient, Entry } from '../types';
 
 const getAll = (): Array<PublicPatient> => {
   return patients.map(({ id, name, dateOfBirth, gender, occupation, entries }) => (
@@ -27,8 +27,18 @@ const getPatient = ( id: string ): Patient | undefined => {
   return patients.find(p => p.id === id);
 };
 
+const addEntry = (id: string, entry: Entry): Patient => {
+  const patient = patients.find(p => p.id === id);
+  if (!patient) {
+    throw Error('invalid patient');
+  }
+  patient.entries.push(entry);
+  return patient;
+};
+
 export default {
   getAll,
   addPatient,
-  getPatient
+  getPatient,
+  addEntry
 };
