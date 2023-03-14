08f1f45fc1f850fb0891ee34cb15f48373234cc7
matnreyes <matias_reyes@icloud.com>
2023-03-13 21:33:02 -0400

    add query to retrieve all books from server

--- part8/library/index.js
+++ part8/library/index.js
@@ -90,7 +90,7 @@ const typeDefs = `
   type Book { 
     title: String!
     published: Int!
-    author: Author!
+    author: String!
     id: ID!,
     genres: [String]!
   }
@@ -98,13 +98,15 @@ const typeDefs = `
   type Query {
     bookCount: Int
     authorCount: Int
+    allBooks: [Book]
   }
 `
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 }
 
