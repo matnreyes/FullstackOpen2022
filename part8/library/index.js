6ecde9101b8e3aee72f4e1b78f83431b1bff673f
matnreyes <matias_reyes@icloud.com>
2023-03-14 09:41:11 -0400

    add mutation to add new book

--- part8/library/index.js
+++ part8/library/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('@apollo/server')
 const { startStandaloneServer } = require('@apollo/server/standalone')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -102,6 +103,15 @@ const typeDefs = `
     allBooks(author: String, genre: String): [Book]!
     allAuthors: [Author]
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      published: Int!
+      author: String!
+      genres: [String]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -130,6 +140,13 @@ const resolvers = {
   },
   Author: {
     bookCount: (root) => (books.filter(b => b.author === root.name)).length
+  },
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid()}
+      books = books.concat(book)
+      return book
+    }
   }
 }
 
